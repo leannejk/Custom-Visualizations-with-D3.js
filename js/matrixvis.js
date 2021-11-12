d6139af123d16d910a47d60e621d9d9a63245390
leannejk <leannejk1@gmail.com>
2021-11-12 12:58:09 -0600

    done

--- js/matrixvis.js
+++ js/matrixvis.js
@@ -34,12 +34,12 @@ MatrixVis.prototype.initVis = function(){
         .attr("height", vis.height + vis.margin.top + vis.margin.bottom)
         .style("margin-left", vis.margin.left + "px")
         .append("g")
+        .attr("id" , "rowFather")
         .attr("transform", "translate(" + vis.margin.left + "," + vis.margin.top + ")");
 
-    // vis.svg.append("rect")
-    //     .attr("class", "background")
-    //     .attr("width", vis.width)
-    //     .attr("height", vis.height);
+    vis.tooltip = d3.select('body').append('div')
+        .attr('class', 'tooltip')
+        .style('opacity', 0)
 
     vis.x = d3.scaleBand()
         .domain(d3.range(16))
@@ -83,6 +83,7 @@ MatrixVis.prototype.wrangleData = function(){
         })
 
     vis.currentData = vis.dataVis
+    vis.initialData = vis.dataVis
 
     vis.columnLabels = new Array(16);
     vis.rowLabels = new Array(16);
@@ -104,10 +105,12 @@ MatrixVis.prototype.wrangleData = function(){
         .attr("class", "column")
         .attr("transform", function(d, i) { return "translate(" + vis.x(i) + ")rotate(-90)"; });
 
-
-
-    console.log(vis.dataVis)
-
+    vis.column.append("text")
+        .attr("x", 6)
+        .attr("y",function(d, i) { return vis.y(i)  / 30; })
+        .attr("dy", ".32em")
+        .attr("text-anchor", "start")
+        .text(function(d, i) { return d; });
 
     vis.updateVis();
 }
@@ -120,46 +123,41 @@ MatrixVis.prototype.wrangleData = function(){
 MatrixVis.prototype.updateVis = function(){
     var vis = this;
     vis.counterM = 0;
-    vis.indexM = 0;
+    vis.indexM = -1;
     vis.counterB = 0;
-    vis.indexB = 0;
-
-    vis.dataVis = vis.currentData;
+    vis.indexB = -1;
 
     var select = document.getElementById('sort-chosen');
     var valueSelected = String(select.options[select.selectedIndex].value);
 
     if(valueSelected !== "ab"){
-        vis.dataVis = vis.currentData.sort(function (a,b) {return b[valueSelected] - a[valueSelected]})
+        vis.dataVis = vis.dataVis.sort(function (a,b) {return b[valueSelected] - a[valueSelected]})
     }
-
-    console.log(valueSelected)
-    console.log(vis.dataVis)
+    else{
+        vis.dataVis = vis.dataVis.sort(function (a,b) {return a.name.localeCompare(b.name)})
+    }
 
     //update row labels:
     for (var i = 0; i < 16; i++) {
         vis.rowLabels[i] = vis.dataVis[i]["name"];
     }
 
-    console.log(vis.rowLabels)
-
-    vis.trianglePath = vis.row.selectAll(".triangle-path")
+    vis.trianglePath1 = vis.row.selectAll(".triangle-path1")
         .data(vis.dataVis);
 
-    vis.trianglePath.enter().append("path")
-        .attr("class", "triangle-path")
-        .merge(vis.trianglePath)
-        .transition(3000)
-        .attr("d", function(d, index) {
-        var x = (vis.cellWidth + vis.cellPadding) * index;
-        var y = 0;
+    vis.path1 = vis.trianglePath1.enter().append("path")
+        .attr("class", "triangle-path1")
+        .merge(vis.trianglePath1)
 
-        return 'M ' + x +' '+ y + ' l ' + vis.cellWidth + ' 0 l 0 ' + vis.cellHeight + ' z';
-    })
+    vis.path1.transition(10000)
+        .attr("d", function(d, index) {
+            var x = (vis.cellWidth + vis.cellPadding) * index;
+            var y = 0;
+
+            return 'M ' + x +' '+ y + ' l ' + vis.cellWidth + ' 0 l 0 ' + vis.cellHeight + ' z';
+        })
         .attr("fill", function (d,i){
-            vis.counterM++;
-            if(vis.counterM === 17){
-                vis.counterM = 0;
+            if(i === 0){
                 vis.indexM++;
             }
             if(vis.dataVis[vis.indexM]["marriageValues"][i] == "1"){
@@ -167,19 +165,51 @@ MatrixVis.prototype.updateVis = function(){
             } else return "#efeff0"
         })
 
-    vis.trianglePath.enter().append("path")
+
+        vis.path1
+            .on('mouseover', function(event, d) {
+                var index = d["index"]
+
+                 vis.svg.selectAll('path')
+                .attr('style', "opacity : 0.2")
+
+                var children = event["path"][1].childNodes
+                for (var i = 0 ; i < children.length; i++){
+                    children[i].style.opacity = "1";
+                }
+
+                var rows = document.getElementById("rowFather").childNodes
+                for(var j = 0; j < 16; j ++){
+                    var child = rows[j].childNodes
+                    child[index].style.opacity = "1"
+                    child[index + 16].style.opacity = "1"
+
+                }
+            vis.tooltip.transition().duration(5000).style('opacity', 0.9)
+        })
+            .on('mouseout', function (){
+                vis.svg.selectAll('path').attr('style', "opacity : 1")
+                vis.tooltip.transition().duration(5000).style('opacity', 0)
+            })
+
+    vis.trianglePath1.exit().remove();
+
+    vis.trianglePath2 = vis.row.selectAll(".triangle-path2")
+        .data(vis.dataVis);
+
+    vis.path2 = vis.trianglePath2.enter().append("path")
         .attr("class", "triangle-path2")
-        .merge(vis.trianglePath)
-        .transition(3000)
+        .merge(vis.trianglePath2)
+
+
+    vis.path2.transition(10000)
         .attr("d", function(d, index) {
             var x = (vis.cellWidth + vis.cellPadding) * index;
             var y = 0;
             return 'M ' + x +' '+ y + ' l 0 ' + vis.cellHeight + ' l ' + vis.cellWidth + ' 0 z';
         })
         .attr("fill", function (d,i){
-            vis.counterB++;
-            if(vis.counterB === 17){
-                vis.counterB = 0;
+            if(i === 0){
                 vis.indexB++;
             }
             if(vis.dataVis[vis.indexB]["businessValues"][i] == "1"){
@@ -187,27 +217,64 @@ MatrixVis.prototype.updateVis = function(){
             } else return "#efeff0"
         })
 
-    vis.trianglePath.exit().remove();
+
+    vis.path2
+        .on('mouseover', function(event, d) {
+            var index = d["index"]
+
+            vis.svg.selectAll('path')
+                .attr('style', "opacity : 0.2")
+
+            var children = event["path"][1].childNodes
+            for (var i = 0 ; i < children.length; i++){
+                children[i].style.opacity = "1";
+            }
+
+            var rows = document.getElementById("rowFather").childNodes
+            for(var j = 0; j < 16; j ++){
+                var child = rows[j].childNodes
+                child[index].style.opacity = "1"
+                child[index + 16].style.opacity = "1"
+
+            }
+            vis.tooltip.transition().duration(5000).style('opacity', 0.9)
+        })
+        .on('mouseout', function (){
+            vis.svg.selectAll('path').attr('style', "opacity : 1")
+            vis.tooltip.transition().duration(5000).style('opacity', 0)
+        })
+
+    vis.trianglePath2.exit().remove();
+
+    // vis.textRow = vis.row.selectAll("text")
+    //     .data(vis.dataVis)
+    //
+    // vis.textRow.enter().append("text")
+    //     .attr("x", 0)
+    //     .attr("dy", ".32em")
+    //     .attr("text-anchor", "end")
+    //     .merge(vis.textRow)
+    //     .attr("y", function(d, i) { return vis.y(i) / 30; })
+    //     .text(function(d, i) { return vis.rowLabels[i] });
+    //
+    // vis.textRow.exit().remove()
 
     //delete previous text:
-    var element = Array.prototype.slice.call(document.getElementsByTagName("text"),0);
+    var element = Array.prototype.slice.call(document.getElementsByClassName("rowText"),0);
 
     for (var index = 0, len = element.length; index < len; index++) {
         element[index].parentNode.removeChild(element[index]);
     }
 
     vis.row.append("text")
+        .attr("class", "rowText")
         .attr("x", 0)
         .attr("y", function(d, i) { return vis.y(i) / 30; })
         .attr("dy", ".32em")
         .attr("text-anchor", "end")
         .text(function(d, i) { return vis.rowLabels[i] });
 
-    vis.column.append("text")
-        .attr("x", 6)
-        .attr("y",function(d, i) { return vis.y(i)  / 30; })
-        .attr("dy", ".32em")
-        .attr("text-anchor", "start")
-        .text(function(d, i) { return d; });
+
+
 
 }
